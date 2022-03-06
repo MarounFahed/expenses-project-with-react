7098c300b60e9be7418cbc26f85e43249100b524
MarounFahed <maroun.fahed6@gmail.com>
2022-03-06 23:54:27 +0200

    React State & Working with Events

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Expenses from './components/Expenses/Expenses'
+import NewExpense from './components/NewExpense/NewExpense'
 
 const App = () => {
   const expenses = [
@@ -28,9 +29,14 @@ const App = () => {
       date: new Date(2022, 2, 1),
     },
   ]
+
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js:  ', expense)
+  }
+
   return (
     <div>
-      <h1>Let's Start!</h1>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   )
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../UI/Card'
 import ExpenseItem from './ExpenseItem'
+import ExpenseFilter from './ExpenseFilter'
 import './Expenses.css'
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2021')
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear)
+  }
+
   return (
     <Card className='expenses'>
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
